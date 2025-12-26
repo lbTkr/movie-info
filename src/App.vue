049d5746247a84cfc3bb3514066091e2f082a5be
강희주 <k.lbtkr@gmail.com>
2025-12-26 19:34:19 +0900

    Vue Course

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,125 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar />
+
+  <h1>영화정보</h1>
+  <div v-for="(movie, i) in data" :key="i" class="item">
+    <figure>
+      <img :src="`${movie.imgUrl}`" :alt="movie.title">
+    </figure>
+
+    <div class="info">
+      <h3 class="bg-yellow">{{ movie.title }}</h3>
+      <p>개봉: {{ movie.year }}</p>
+      <p>장르: {{ movie.category }}</p>
+      <button @:click="increaseLike(i)">좋아요</button><span>{{ movie.like }}</span>
+
+      <p>
+        <button @click="isModal=true; selectedMovie=i; showValue(i)">상세보기</button>
+      </p>
+    </div>
+  </div>
+
+  <Modal />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import data from './assets/movies'; // 영화 데이터
+  import Navbar from './components/Navbar.vue';
+  import Modal from './components/Modal.vue';
+  
+  export default {
+    name: 'App', //컴포넌트명
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    //문서에 표시될 변수 정의
+    data(){
+      return {//상태변수: 화면에 자주 업데이트 되는 내용들을 의미
+        isModal: false,
+        data : data,
+        selectedMovie: 0,
+      }
+    },
+
+    //내용이 길어질 경우, methods로 따로 빼서 함수 작성
+    methods:{
+      increaseLike(i){
+        this.data[i].like += 1; //this를 작성해야 객체 안에 있는 like를 찾아서 함수를 실행함
+      },
+      showValue(val){
+        console.log(`val === ${val}`);
+      },
+    },
+
+    components: {
+      Navbar: Navbar,
+      Modal: Modal,
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+body {
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+h1,
+h2,
+h3 {
+  margin-bottom: 1rem;
+}
+
+p {
+  margin-bottom: 0.5rem;
+}
+
+button {
+  margin-right: 10px;
+  margin-top: 1rem;
+}
+
+.item {
+  width: 100%;
+  border: 1px solid #ccc;
+  display: flex;
+  margin-bottom: 20px;
+  padding: 1rem;
+}
+
+.item figure {
+  width: 30%;
+  margin-right: 1rem;
+}
+
+.item img {
+  width: 100%;
+}
+
+.item .info {
+  width: 100%;
+}
+
+.modal {
+  background: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal .inner {
+  background: #fff;
+  width: 80%;
+  padding: 20px;
+  border-radius: 10px;
 }
 </style>
---
+++ src/assets/movies.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+    title: "액시트",
+    year: 2023,
+    category: "코미디, 재난, 드라마",
+    like: 0,
+    imgUrl: "https://img.khan.co.kr/news/2019/07/30/l_2019073001003557100286352.jpg",
+    },
+    {
+    title: "파묘",
+    year: 2024,
+    category: "드라마, 오컬트, 공포",
+    like: 0,
+    imgUrl: "https://design.co.kr/wp-content/uploads/2024/03/07_%ED%8C%8C%EB%AC%98_%EC%8A%A4%ED%8E%98%EC%85%9C-%ED%8F%AC%EC%8A%A4%ED%84%B0_%EC%A0%84%EB%8B%AC%EC%9A%A9-832x1189.jpg",
+    },
+    {
+    title: "부산행",
+    year: 2021,
+    category: "좀비, 재난, 아포칼립스",
+    like: 0,
+    imgUrl: "https://mblogthumb-phinf.pstatic.net/MjAxNzAyMjJfNSAg/MDAxNDg3NzI0MjQzNzM5.1lVCSNpeV_wLCrkAXolwhHixQYgpetlH29HQcWUY1t4g.-LlMVsqtbJfQvWAadQCF6XpLucYQ6QQVi7edISl8mqAg.JPEG.designpress2016/%EB%B6%80%EC%82%B0%ED%96%89_5.jpg?type=w800",
+    },
+    
+]
+
+export default data; //export default = 여러 가지를 보내는게 아니라 하나만 대표로 보낸다 라는 의미
